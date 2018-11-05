c692fc4cc0f56182dc51d42da67574d6702c41e6
prabin pun <prabin1punah@gmail.com>
2018-11-04 22:36:03 -0500

    Add project files.

---
+++ KeyValue/KeyValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyValueStore
+{
+    class KeyValue<T>
+    {
+        public string key { get; }
+        public T value { get; }
+
+        public KeyValue(string key, T value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
---
+++ KeyValue/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyValueStore
+{
+    class MyDictionary<T>
+    {
+        public KeyValue<T>[] keyValueArr = new KeyValue<T>[50];
+        int counter = -1;
+
+        public Object this[string key]
+        {
+            get
+            {
+                for (int i = 0; i < keyValueArr.Length; i++)
+                {
+                    if (key == keyValueArr[i].key)
+                    {
+                        return keyValueArr[i].value;
+                    }
+                }
+                throw new KeyNotFoundException();
+            }
+            set
+            {
+                bool replace = false;
+                for (int i = 0; i < counter; i++)
+                {
+                    if (key == keyValueArr[i].key)
+                    {
+                        keyValueArr[i] = new KeyValue<T>(key, keyValueArr[i].value);
+                        replace = true;
+                    }
+                }
+                if (replace == false)
+                {
+                    keyValueArr[counter + 1] = new KeyValue<T>(key, value);
+                }
+                counter++;
+            }
+        }
+    }
+}
